47386f1608f87859183932304fff75865e83c49d
Swassyman <swassyman2005@gmail.com>
2025-09-19 22:23:18 +0530

    some changes before i potentially experiment with light

--- Scripts/texture.gd
+++ Scripts/texture.gd
@@ -2,12 +2,37 @@ extends TextureRect
 
 @export var cam_size: Vector2 = Vector2(256, 256)
 
-func _ready() -> void:
-	pass
+var player: CharacterBody2D
+var mask_rect: ColorRect
 
+var is_locked = false
+var lock_pos: Vector2
+
+func _ready() -> void:
+	player = get_node("../Player")
 
 func _process(delta: float) -> void:
-	pass
+	if Input.is_action_just_pressed("lock"):
+		toggle_lock()
+		
+	var pivot_pos = player.global_position
+	
+	if not is_locked and player:
+		global_position = pivot_pos - size / 2
+	elif is_locked:
+		global_position = lock_pos
+		
+	if mask_rect:
+		var mat = mask_rect.material as ShaderMaterial
+		mat.set_shader_parameter("rect_pos", global_position)
+		mat.set_shader_parameter("rect_size", size)
 
+
+func toggle_lock():
+	is_locked = !is_locked
+	
+	if is_locked:
+		lock_pos = global_position
+		
 func get_warp_bounds() -> Rect2:
 	return Rect2(global_position, size)
